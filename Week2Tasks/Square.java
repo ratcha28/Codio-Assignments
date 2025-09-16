7cde11d14debe1273ce01c44f438ff06e8027b4e
ratcha28 <ratchatvm@gmail.com>
2025-09-16 19:42:06 +0530

    Squares of number

---
+++ Week2Tasks/Square.java
@@ -0,0 +1,9 @@
+public class Square {
+    public static void main(String[] args) {
+        int val=0;
+        for(int i=1;i<=15;i++){
+            val=i*i;
+            System.out.println(val);
+        }
+    }
+}
