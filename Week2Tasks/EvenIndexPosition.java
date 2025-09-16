96874c8cf2f24a60bdf41675f7f1be84b36ea8b2
ratcha28 <ratchatvm@gmail.com>
2025-09-16 19:40:56 +0530

    Even Index position

---
+++ Week2Tasks/EvenIndexPosition.java
@@ -0,0 +1,9 @@
+public class EvenIndexPosition {
+    public static void main(String[] args) {
+        int []arr={10,20,30,40,50,60};
+        for(int i=0;i<arr.length;i++){
+            if(i%2==0)
+                System.out.print(" "+arr[i]);
+        }
+    }
+}
